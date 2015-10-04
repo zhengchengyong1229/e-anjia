3940e63b94dfc351370295165dd65ef3d49e3b9b
LEE <smartymoon@qq.com>
2015-10-04 10:10:45 +0800

    change

--- Application/Home/Model/PropertyModel.class.php
+++ Application/Home/Model/PropertyModel.class.php
@@ -21,9 +21,11 @@ class PropertyModel extends Model{
      *
      */
     public function getlist($map,$page = 0 ,$order = 'objectcount desc', $r = 10 ){
-        $field = 'distinct a.cuxiao,a.id,a.name as title,a.type,a.objectcount,a.uptime,a.cover,(select min(`totalprice`/`area`*10000) from fang_object where fid = a.id) as uprice';
+        $field = 'distinct a.cuxiao,a.id,a.name as title,a.type,a.objectcount,a.uptime,a.cover,(select min(`totalprice`/`area`*10000) from fang_object where fid = a.id) as uprice,CONCAT(f.name,e.name) as area';
         $list  =     $this->where($map)->field($field)->alias('a')
                      ->join('__OBJECT__ d on d.fid = a.id','left')   //房源搜索条件
+                     ->join('__CBD__  e on a.pid = e.id','left')   //房源搜索条件
+                     ->join('__CBD__  f on a.district = f.id','left')   //房源搜索条件
                      //->order('a.objectcount desc')
                      ->order('a.uptime desc')
                      ->page($page,$r)
